0336ba222eb942990d3ac21bcb24c8cf90d38825
JESSYSAYAS <jessysayas15@gmail.com>
2022-12-13 19:24:01 -0500

    thyrew

--- condicion.php
+++ condicion.php
@@ -70,8 +70,8 @@ include "include/verificar_sesion.php";
                           <td><?php echo $res_b_cargo['descripcion']; ?></td>
                           <td><?php echo $res_b_cargo['codigo']; ?></td>
                           <td>
-                            <a href="editar_docente.php?id=<?php echo $res_b_cargo['id']; ?>" class="btn btn-primary">Editar</a>
-                            <a href="operaciones/eliminar_cargo.php?id=<?php echo $res_b_cargo['descripcion']; ?>" class="btn btn-danger">Eliminar</a>
+                            <a href="editar_condicion.php?id=<?php echo $res_b_cargo['id']; ?>" class="btn btn-primary">Editar</a>
+                            <a href="operaciones/eliminar_condicion.php?id=<?php echo $res_b_cargo['descripcion']; ?>" class="btn btn-danger">Eliminar</a>
                           </td>
                         </tr>
                         <?php
