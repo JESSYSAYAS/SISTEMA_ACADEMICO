092d832da3219ca5bcc625552ea3a70e75246fca
JESSYSAYAS <jessysayas15@gmail.com>
2022-12-13 21:19:55 -0500

    Update login.php

--- login.php
+++ login.php
@@ -26,12 +26,12 @@
     <div>
       <a class="hiddenanchor" id="signup"></a>
       <a class="hiddenanchor" id="signin"></a>
-
-      <div class="login_wrapper">
-      <div class="profile_pic">
+            <div class="profile_pic">
                 <img src="./img/logo.png" alt="..." class="img-circle profile_img">
               </div>
               <div class="profile_info">
+      <div class="login_wrapper">
+      
         <div class="animate form login_form">
           <section class="login_content">
             <form method="POST" action="operaciones/iniciar_sesion.php">
