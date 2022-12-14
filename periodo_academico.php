e3443ba841acb234cd227f7f75adaa92bdbdbeb2
JESSYSAYAS <jessysayas15@gmail.com>
2022-12-13 22:31:58 -0500

    seee

--- periodo_academico.php
+++ periodo_academico.php
@@ -40,11 +40,12 @@ include "include/verificar_sesion.php";
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>PERIODO ACADEMICO</h2>
+                    <h2><font color="purple" size="30px" face="Star Wars">PERIODO ACADEMICO</font></h2>
                     
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
+                  <div class="x_content"><font color="purple" face="Star Wars" >
                     <br />
                     <form class="form-horizontal form-label-left" method="POST" action="operaciones/registrar_periodo.php">
 
@@ -100,6 +101,7 @@ include "include/verificar_sesion.php";
                       </div>
 
                     </form>
+                    </font>
                   </div>
                 </div>
               </div>
