32d73070cc599082aba158c3f95bf61d0737960e
JESSYSAYAS <jessysayas15@gmail.com>
2022-12-15 01:55:23 -0500

    este

--- include/conexion.php
+++ include/conexion.php
@@ -1,9 +1,9 @@
 <?php
 // conexion utiliza servidor-usuario-password-base de datos
-$conexion = mysqli_connect("localhost", "root", "", "sistema_academico-l");
+$conexion = mysqli_connect("alhost", "desarro1_lizbeth", "VFS7ro5f]q00", "desarro1_Sayasliz");
 if ($conexion) {
 
 } else {
    echo "conexion fallida";
 }
-?>
+?>}
